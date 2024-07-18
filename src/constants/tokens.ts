ce0d6b9c9ee54459f1afcee15671299cdf908980
clement <hecquet.c59@gmail.com>
2024-07-19 01:23:45 +0200

    Added images for NFTs and Tokens

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -3,6 +3,7 @@ export interface Token {
   name: string;
   symbol: string;
   decimals: number;
+  image: string | null;
 }
 
 export interface Tokens {
@@ -15,7 +16,29 @@ export const TOKENS: Tokens = {
       contractAddress: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
       name: "USDC",
       symbol: "USDC",
-      decimals: 6
+      decimals: 6,
+      image: "https://static.debank.com/image/coin/logo_url/usdc/e87790bfe0b3f2ea855dc29069b38818.png"
+    },
+    {
+      contractAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      name: "WETH",
+      symbol: "WETH",
+      decimals: 18,
+      image: "https://static.debank.com/image/eth_token/logo_url/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2/61844453e63cf81301f845d7864236f6.png"
+    },
+    {
+      contractAddress: "0x0000000000000000000000000000000000000000",
+      name: "ETH",
+      symbol: "ETH",
+      decimals: 18,
+      image: "https://static.debank.com/image/chain/logo_url/eth/42ba589cd077e7bdd97db6480b0ff61d.png"
+    },
+    {
+      contractAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      name: "USDT",
+      symbol: "USDT",
+      decimals: 18,
+      image: "https://static.debank.com/image/coin/logo_url/usdt/23af7472292cb41dc39b3f1146ead0fe.png"
     },
   ],
   34443: [
@@ -23,61 +46,71 @@ export const TOKENS: Tokens = {
       contractAddress: "0x0000000000000000000000000000000000000000",
       name: "ETH",
       symbol: "ETH",
-      decimals: 18
+      decimals: 18,
+      image: "https://static.debank.com/image/chain/logo_url/eth/42ba589cd077e7bdd97db6480b0ff61d.png"
     },
     {
       contractAddress: "0xd988097fb8612cc24eeC14542bC03424c656005f",
       name: "USDC",
       symbol: "USDC",
-      decimals: 6
+      decimals: 6,
+      image: "https://static.debank.com/image/coin/logo_url/usdc/e87790bfe0b3f2ea855dc29069b38818.png"
     },
     {
       contractAddress: '0xcDd475325D6F564d27247D1DddBb0DAc6fA0a5CF',
       symbol: 'WBTC',
       name: 'Wrapped BTC',
-      decimals: 8
+      decimals: 8,
+      image: "https://static.debank.com/image/eth_token/logo_url/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599/d3c52e7c7449afa8bd4fad1c93f50d93.png",
     },
     {
       contractAddress: '0xf0F161fDA2712DB8b566946122a5af183995e2eD',
       symbol: 'USDT',
       name: 'USDT',
-      decimals: 6
+      decimals: 6,
+      image: "https://static.debank.com/image/coin/logo_url/usdt/23af7472292cb41dc39b3f1146ead0fe.png"
     },
     {
       contractAddress: '0xDfc7C877a950e49D2610114102175A06C2e3167a',
       symbol: 'MODE',
       name: 'Mode',
-      decimals: 18
+      decimals: 18,
+      image: 'https://static.debank.com/image/chain/logo_url/mode/466e6e12f4fd827f8f497cceb0601a5e.png'
     },
     {
       contractAddress: '0x71ef7EDa2Be775E5A7aa8afD02C45F059833e9d2',
       symbol: 'ionWETH',
       name: 'Ionic Wrapped Ether',
-      decimals: 18
+      decimals: 18,
+      image: null,
     },
     {
       contractAddress: '0x4200000000000000000000000000000000000006',
       symbol: 'WETH',
       name: 'Wrapped Ether',
-      decimals: 18
+      decimals: 18,
+      image: 'https://static.debank.com/image/mtr_token/logo_url/0x79a61d3a28f8c8537a3df63092927cfa1150fb3c/61844453e63cf81301f845d7864236f6.png'
     },
     {
       contractAddress: '0x59e710215d45F584f44c0FEe83DA6d43D762D857',
       symbol: 'ionezETH',
       name: 'Ionic Renzo Restaked ETH',
-      decimals: 18
+      decimals: 18,
+      image: null
     },
     {
       contractAddress: '0x2416092f143378750bb29b79eD961ab195CcEea5',
       symbol: 'ezETH',
       name: 'Renzo Restaked ETH',
-      decimals: 18
+      decimals: 18,
+      image: 'https://static.debank.com/image/mode_token/logo_url/0x2416092f143378750bb29b79ed961ab195cceea5/a66d77b85dfd99539744bc62966e1fac.png'
     },
     {
       contractAddress: '0x9c29a8eC901DBec4fFf165cD57D4f9E03D4838f7',
       symbol: 'ironETH',
       name: 'Ironclad ETH',
-      decimals: 18
+      decimals: 18,
+      image: null
     },
   ]
 };
@@ -85,6 +118,7 @@ export const TOKENS: Tokens = {
 export interface NFT {
   contractAddress: string;
   name: string;
+  image: string | null;
 }
 
 export interface NFTs {
@@ -95,31 +129,37 @@ export const NFTS: NFTs = {
   1: [
     {
       contractAddress: "0x60e4d786628fea6478f785a6d7e704777c86a7c6",
-      name: "MutantApeYachtClub"
+      name: "MutantApeYachtClub",
+      image: "https://images.blur.io/_blur-prod/0x60e4d786628fea6478f785a6d7e704777c86a7c6/4647-3c64c4804b4cd5fe?w=128"
     },
     {
       contractAddress: "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d",
-      name: "BoredApeYachtClub"
+      name: "BoredApeYachtClub",
+      image: "https://images.blur.io/_blur-prod/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d/8321-69a4c46a6e8e5b07?w=128"
     },
 
     {
       contractAddress: "0xbd3531da5cf5857e7cfaa92426877b022e612cf8",
-      name: "Pudgy Penguins"
+      name: "Pudgy Penguins",
+      image: "https://images.blur.io/_blur-prod/0xbd3531da5cf5857e7cfaa92426877b022e612cf8/760-b91995265fdeb95a?w=128"
     },
     {
       contractAddress: "0x8a90cab2b38dba80c64b7734e58ee1db38b8992e",
-      name: "Doodles"
+      name: "Doodles",
+      image: "https://images.blur.io/_blur-prod/0x8a90cab2b38dba80c64b7734e58ee1db38b8992e/7522-a0e3ae3f8d77961e?w=128"
     },
     {
       contractAddress: "0x524cab2ec69124574082676e6f654a18df49a048",
-      name: "Lil Pudgies"
+      name: "Lil Pudgies",
+      image: "https://images.blur.io/_blur-prod/0x524cab2ec69124574082676e6f654a18df49a048/17176-05eebb761a566d53?w=128"
     },
 
   ],
   34443: [
     {
       contractAddress: "0x2ad86eeec513ac16804bb05310214c3fd496835b",
-      name: "Space id"
+      name: "Space id",
+      image: "https://s.nfte.so/asset/collection/featured/4e4e0173-77d8-42fd-86a5-b5ab4a6ac394.png?x-oss-process=image/resize,m_fill,w_300,h_300,type_6/ignore-error,1"
     }
   ]
 };
