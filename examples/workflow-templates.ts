e027d737694fa87a3be70bb069ef8ba3f9bb83da
clement <hecquet.c59@gmail.com>
2024-07-29 13:11:05 +0200

    Added tags to templates

--- examples/workflow-templates.ts
+++ examples/workflow-templates.ts
@@ -1,4 +1,5 @@
-import { WORKFLOW_TEMPLATES } from '../src/index.js';
+import { WORKFLOW_TEMPLATES, WORKFLOW_TEMPLATES_TAGS } from '../src/index.js';
 
 const workflow = WORKFLOW_TEMPLATES[1].createWorkflow()
 console.log(workflow)
+console.log(WORKFLOW_TEMPLATES_TAGS)
--- src/constants/WorkflowTemplates.ts
+++ src/constants/WorkflowTemplates.ts
@@ -1,5 +1,12 @@
 import { Workflow, Trigger, Action, Edge, TRIGGERS, ACTIONS, CHAINS, getTokenFromSymbol } from '../index.js';
 
+export const WORKFLOW_TEMPLATES_TAGS = {
+    NFTS: 'NFTs',
+    SOCIALS: 'Socials',
+    TRADING: 'Trading',
+    ON_CHAIN_MONITORING: 'On-chain monitoring'
+};
+
 const createModeTransferNotificationWorkflow = () => {
     const modeTransferTrigger = new Trigger(TRIGGERS.TOKENS.ERC20.TRANSFER);
 
@@ -21,24 +28,25 @@ const createETHFearAndGreedBuy = () => {
     trigger.setCondition('lt');
     trigger.setComparisonValue(30);
 
-    // todo: change to swap
     const telegramAction = new Action(ACTIONS.NOTIFICATIONS.TELEGRAM.SEND_MESSAGE);
-    telegramAction.setParams("message", "0x74B8....C6B4 transferred $MODE");
+    telegramAction.setParams("message", "The market sentiment is extremely fearful. Consider buying ETH.");
 
     const edge = new Edge({ source: trigger, target: telegramAction });
 
-    return new Workflow('Buy ETH when the market sentiment is fearful', [trigger, telegramAction], [edge]);
+    return new Workflow('Buy ETH when the market sentiment is extremely fearful', [trigger, telegramAction], [edge]);
 }
 
 export const WORKFLOW_TEMPLATES = [
     {
         'name': 'MODE transfer notification',
         'description': 'Receive notifications when a top $MODE holder (0x74B8....C6B4) transfers $MODE',
+        'tags': [WORKFLOW_TEMPLATES_TAGS.ON_CHAIN_MONITORING],
         createWorkflow: createModeTransferNotificationWorkflow
     },
     {
-        'name': 'Buy ETH when the market sentiment is fearful',
-        'description': 'Buy eth when the bitcoin fear and greed is below 30',
+        'name': 'Buy ETH when the market sentiment is extremely fearful',
+        'description': 'Buy ETH when the Bitcoin Fear and Greed Index is below 30',
+        'tags': [WORKFLOW_TEMPLATES_TAGS.TRADING, WORKFLOW_TEMPLATES_TAGS.SOCIALS],
         createWorkflow: createETHFearAndGreedBuy
     },
-]
+];
