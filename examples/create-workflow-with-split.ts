60909bede11bac0e0bb10a11bc730a33d21d4260
clement <hecquet.c59@gmail.com>
2024-10-30 20:33:29 +0100

    temp commit

---
+++ examples/create-workflow-with-split.ts
@@ -0,0 +1,63 @@
+import { ACTIONS, Action, TRIGGERS, Trigger, Workflow, CHAINS, getTokenFromSymbol, Edge, apiServices, convertToTokenUnits, convertToTokenUnitsFromSymbol } from '../src/index.js';
+import dotenv from 'dotenv';
+
+// Load environment variables from the .env file
+dotenv.config();
+
+async function main() {
+    if (!process.env.API_URL || !process.env.AUTH_TOKEN)
+        return;
+
+    apiServices.setUrl(process.env.API_URL);
+    apiServices.setAuth(process.env.AUTH_TOKEN);    
+    
+    const trigger = new Trigger(TRIGGERS.TOKENS.ON_CHAIN_PRICE_MOVEMENT.PRICE_MOVEMENT_AGAINST_CURRENCY);
+    trigger.setChainId(CHAINS.MODE);
+    trigger.setComparisonValue(3000);
+    trigger.setCondition('lte');
+    trigger.setParams('currency', 'USD');
+    trigger.setContractAddress(getTokenFromSymbol(CHAINS.MODE, 'WETH').contractAddress);
+    trigger.setPosition(0, 0);
+
+    const split = new Action(ACTIONS.CORE.SPLIT.SPLIT);
+
+    const workflow = new Workflow("swap & deposit", [trigger, split, slack1, slack2]);
+
+    const edge = new Edge({
+        source: trigger,
+        target: odosAction,
+    });
+    const edge2 = new Edge({
+        source: odosAction,
+        target: ionicDeposit,
+    });
+    const edge3 = new Edge({
+        source: odosAction,
+        target: ionicDeposit,
+    });
+
+    workflow.addEdge(edge);
+    workflow.addEdge(edge2);
+
+    console.log(JSON.stringify(workflow.toJSON()))
+
+    const creationResult = await workflow.create();
+    console.log(workflow.getState());
+
+    if (!creationResult.success) {
+        throw new Error("An error occurred when publishing the workflow")
+    }
+
+    console.log(workflow.id);
+
+    const runResult = await workflow.run();
+    console.log(workflow.getState());
+
+    if (!runResult.success) {
+        throw new Error("An error occurred when running the workflow")
+    } else {
+        console.log('workflow is running');
+    }
+}
+
+main();
