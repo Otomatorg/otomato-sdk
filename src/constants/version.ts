d638d6f15755022cb6d6c7a89d769afc607d600e
clement <hecquet.c59@gmail.com>
2025-01-20 16:25:47 +0100

    Updated version

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION = '2.0.26';
+export const SDK_VERSION = '2.0.27';
 
 export function compareVersions(v1: string, v2: string): number {
     // Split the version strings into parts
