4b7f3544917c7861ad748e3842141daaa669820e
clement <hecquet.c59@gmail.com>
2025-02-04 23:22:58 +0100

    Rmeove duseless imports

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,4 +1,4 @@
-export const SDK_VERSION = '2.0.61';
+export const SDK_VERSION = '2.0.62';
 
 export function compareVersions(v1: string, v2: string): number {
     // Split the version strings into parts
