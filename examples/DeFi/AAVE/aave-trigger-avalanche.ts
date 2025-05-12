c31f4d4be6a8fa69a0d0798b1f56c652b9500b40
Thuan Vo <thuan.vo@aegona.com>
2025-05-12 10:16:52 +0700

    Support Avalanche, Sonic, Optimism, Polygon, Binance for Aave

---
+++ examples/DeFi/AAVE/aave-trigger-avalanche.ts
@@ -0,0 +1,59 @@
+import { ACTIONS, Action, TRIGGERS, Trigger, Workflow, CHAINS, getTokenFromSymbol, Edge, apiServices, LOGIC_OPERATORS, ConditionGroup, convertToTokenUnitsFromSymbol, rpcServices } from '../../../src/index.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function aave_trigger_avalanche() {
+
+  if (!process.env.API_URL || !process.env.AUTH_TOKEN)
+    return;
+
+  apiServices.setUrl(process.env.API_URL);
+  apiServices.setAuth(process.env.AUTH_TOKEN); 
+
+  // -------- Supply rate trigger --------
+  const aaveTrigger = new Trigger(TRIGGERS.LENDING.AAVE.LENDING_RATE);
+  aaveTrigger.setChainId(CHAINS.AVALANCHE);
+  aaveTrigger.setParams(
+    "abiParams.asset",
+    getTokenFromSymbol(CHAINS.AVALANCHE, "WETH.e").contractAddress  // WAVAX, USDC, WETH, AUSD, DAI.e, LINK.e, AAVE.e
+  );
+  aaveTrigger.setParams(
+    "condition",
+    "gt"
+  );
+  // Set as 0 to ensure the trigger is triggered instantly
+  aaveTrigger.setParams(
+    "comparisonValue",
+    0
+  );
+  
+  // -------- Wait 1 second as we only focus on the trigger --------
+  const waitAction = new Action(ACTIONS.CORE.DELAY.WAIT_FOR);
+  waitAction.setParams("time", '1000');
+
+  const edge1 = new Edge({
+    source: aaveTrigger,
+    target: waitAction,
+  });
+  const workflow = new Workflow(
+    "AAVE Trigger Avalanche",
+    [
+      aaveTrigger,
+      waitAction,
+    ],
+    [edge1]
+  );
+
+  const creationResult = await workflow.create();
+
+  console.log("AAVE Trigger Avalanche before: " + workflow.getState());
+
+  console.log("Workflow ID: " + workflow.id);
+
+  const runResult = await workflow.run();
+
+  console.log("AAVE Trigger Avalanche after: " + workflow.getState());
+}
+
+aave_trigger_avalanche();
